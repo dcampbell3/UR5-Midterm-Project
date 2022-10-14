5f855a3327f23d76308e4e0036ad66f09a3b5ae4
drc49 <drc49@duke.edu>
2022-10-14 13:08:35 -0400

    created run.py

---
+++ run.py
@@ -0,0 +1,11 @@
+from setup import * 
+from D import trace_D
+import sys
+import copy
+import rospy
+import moveit_commander
+import moveit_msgs.msg
+import geometry_msgs.msg
+import math
+
+trace_D()
