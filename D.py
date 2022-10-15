134183211f05883a8815ef05c4cc68b42aedae1e
drc49 <drc49@duke.edu>
2022-10-15 16:18:15 -0400

    updated D

--- D.py
+++ D.py
@@ -8,6 +8,7 @@ import math
 from setup import *
 
 def trace_D():
+    '''
     d = 0.5 / (2*math.cos(math.radians(45))+1)
 
     waypoints = []
@@ -41,5 +42,38 @@ def trace_D():
 
     (plan, fraction) = move_group.compute_cartesian_path(waypoints, 0.01, 0.0) 
     move_group.execute(plan, wait=True)
+    '''
 
+    x_len = 0.25 / (2*math.cos(math.radians(45))+1)
+
+    waypoints = []
+    wpose = move_group.get_current_pose().pose
+    #Move 2: 
+    wpose.position.z -= 0.25
+    waypoints.append(copy.deepcopy(wpose))
+
+    #Move 3: 
+    wpose.position.x -= x_len
+    waypoints.append(copy.deepcopy(wpose))
+
+    #Move 4: 
+    wpose.position.x -= x_len * math.cos(math.radians(45))
+    wpose.position.z += x_len * math.cos(math.radians(45))
+    waypoints.append(copy.deepcopy(wpose))
+
+    #Move 5: 
+    wpose.position.z += x_len
+    waypoints.append(copy.deepcopy(wpose))
+
+    #Move 6: 
+    wpose.position.x += x_len * math.cos(math.radians(45))
+    wpose.position.z += x_len * math.cos(math.radians(45))
+    waypoints.append(copy.deepcopy(wpose))
+
+    #Move 7: 
+    wpose.position.x += x_len
+    waypoints.append(copy.deepcopy(wpose))
+
+    (plan, fraction) = move_group.compute_cartesian_path(waypoints, 0.01, 0.0) 
+    move_group.execute(plan, wait=True)
     return
--- reset.py
+++ reset.py
@@ -11,22 +11,16 @@ from setup import *
 # move_group.go(joint_goal, wait=True)
 # move_group.stop()
 def return_to_start():
-    waypoints = []
-    wpose = move_group.get_current_pose()
+    P1 = [0, -61, 99, 140, 0, -178]
 
-    # final goal 
+    points_list = [P1]  
 
-    wpose.pose.position.x = 0.5119164436695298
-    wpose.pose.position.y =0.19130372208443258
-    wpose.pose.position.z = 0.19130372208443258
-    wpose.pose.orientation.x = 0.707210484455942
-    wpose.pose.orientation.y =0.7070030569705926
-    wpose.pose.orientation.z = 6.376270144183684e-05
-    wpose.pose.orientation.w =6.35936043025721e-05
-    waypoints.append(copy.deepcopy(wpose))
+    joint_goal = move_group.get_current_joint_values()
 
-    (plan, fraction) = move_group.compute_cartesian_path(waypoints, 0.01, 0.0) 
-    move_group.execute(plan, wait=True)
+    for point in points_list: 
+        joint_goal = [math.radians(p) for p in point]
+        move_group.go(joint_goal, wait=True)
+        move_group.stop()
 
     return
 
